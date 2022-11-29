bf121800d1e23d5d2c3aff587e1bf92d3d5b874b
jmjusmin <jmjusmin1412@gmail.com>
2022-11-28 18:22:51 -0800

    Display all product from sanity

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,36 @@
 import React from "react";
+import { client } from "../lib/client";
+import { Product, FooterBanner, HeroBanner } from "../components";
 
-function Home() {
+function Home({ products, bannerData }) {
   return (
     <>
-      HeroBanner
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speakers of many vaiations</p>
       </div>
-      <div className="products-container">{["p1", "p2"].map((p) => p)}</div>
-      Footer
+      <div className="products-container">
+        {products?.map((product) => (
+          <Product key={product._id} product={product} />
+        ))}
+      </div>
+      <FooterBanner footerBanner={bannerData && bannerData[0]} />
     </>
   );
 }
+
+export const getServerSideProps = async () => {
+  //get all the info from product
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData },
+  };
+};
 
 export default Home;
